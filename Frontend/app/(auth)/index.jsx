48a80994af349ceec9d168e85ab4ce8d273c85c1
Patryk Wojewoda <p.wojewoda1998@gmail.com>
2025-04-01 19:22:12 +0200

    Create login interface

--- Frontend/app/(auth)/index.jsx
+++ Frontend/app/(auth)/index.jsx
@@ -1,10 +1,71 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import styles from "../../assets/styles/login.styles";
+import { useState } from "react";
+import { Image } from "expo-image";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <View>
-      <Text>Login Screan</Text>
+    <View style={styles.container}>
+      <View style={styles.topIllustration}>
+        <Image
+          source={require("../../assets/images/WelcomePageMainImage.png")}
+          style={styles.illustrationImage}
+        />
+      </View>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Email</Text>
+        <View style={styles.inputContainer}>
+          <Ionicons
+            name="mail-outline"
+            size={20}
+            color={COLORS.primary}
+            style={styles.inputIcon}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your email"
+            placeholderTextColor={COLORS.placeholderText}
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Ionicons
+            name="lock-closed-outline"
+            size={20}
+            color={COLORS.primary}
+            style={styles.inputIcon}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your password"
+            placeholderTextColor={COLORS.placeholderText}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={!showPassword}
+          />
+          <TouchableOpacity
+            onPress={() => setShowPassword(!showPassword)}
+            style={styles.eyeIcon}
+          >
+            <Ionicons
+              name={showPassword ? "eye-outline" : "eye-off-outline"}
+              size={20}
+              color={COLORS.primary}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
